45bc95fe09bc67208dfb12586b810b60f3a5d58b
Ralakash <shawnkaralash@gmail.com>
2023-03-01 18:31:00 -0500

    fixes async in s3 routes

--- server/controllers/s3Controller.js
+++ server/controllers/s3Controller.js
@@ -23,38 +23,55 @@ const s3 = new S3({
 
 module.exports = {
 	// Uploads file to s3
-	uploadFile(req, res) {
-		const fileStream = fs.createReadStream(req.path);
+	async uploadFile(req, res) {
+		try {
+			const fileStream = fs.createReadStream(req.path);
 
-		const uploadParams = {
-			Bucket: bucketName,
-			Body: fileStream,
-			Key: req.filename,
-		};
-		const result = s3.upload(uploadParams).promise();
-		// this image path will need to be changed just not sure how i want to do it yet
-		return res.send({ imagePath: `/images/${result.key}` });
-	},
-	// downloads a file from s3
-	getFileStream(req, res) {
-		const key = req.params.key;
-		const downloadParams = {
-			Key: key,
-			Bucket: bucketName,
-		};
-		const readStream = s3.getObject(downloadParams).createReadStream();
-		return readStream.pipe(res);
-	},
-	deletePhoto(req, res) {
-		const deleteParams = {
-			Bucket: bucketName,
-			Key: req.params.key,
-		};
+			const uploadParams = {
+				Bucket: bucketName,
+				Body: fileStream,
+				Key: req.filename,
+			};
 
-		return s3.deleteObject(deleteParams, function (err, data) {
-			if (err) console.log(err, err.stack);
-			else console.log(data);
-		});
+			const result = await s3.upload(uploadParams).promise();
+
+			return res.send({ imagePath: `/images/${result.key}` });
+		} catch (error) {
+			console.error(error);
+			return res.status(500).send({ error: 'Failed to upload file' });
+		}
+	},
+
+	async getFileStream(req, res) {
+		try {
+			const key = req.params.key;
+			const downloadParams = {
+				Key: key,
+				Bucket: bucketName,
+			};
+
+			const readStream = s3.getObject(downloadParams).createReadStream();
+			return readStream.pipe(res);
+		} catch (error) {
+			console.error(error);
+			return res.status(500).send({ error: 'Failed to download file' });
+		}
+	},
+
+	async deletePhoto(req, res) {
+		try {
+			const deleteParams = {
+				Bucket: bucketName,
+				Key: req.params.key,
+			};
+
+			await s3.deleteObject(deleteParams).promise();
+
+			return res.send({ message: 'File deleted successfully' });
+		} catch (error) {
+			console.error(error);
+			return res.status(500).send({ error: 'Failed to delete file' });
+		}
 	},
 };
 
