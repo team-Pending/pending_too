0be511ce4485c03c1c504a404228465f3b05752b
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2023-03-01 19:56:54 -0500

    refactored package.jsons for concurrently, added components and some utils for stripe.

---
+++ client/src/utils/reducers.jsx
@@ -0,0 +1,91 @@
+import { useReducer } from "react";
+import {
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  CLEAR_CART,
+  REMOVE_FROM_CART,
+  TOGGLE_CART,
+  UPDATE_CART_QUANTITY,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  UPDATE_PRODUCTS,
+} from "./actions";
+
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
