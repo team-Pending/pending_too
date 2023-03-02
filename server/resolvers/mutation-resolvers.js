4e4ca7fc982fcfbf18e8be51fbfc4ddfa0bb1027
Ralakash <shawnkaralash@gmail.com>
2023-03-01 19:48:23 -0500

    adds a bunch of files to get me started on s3 stuff

---
+++ server/resolvers/mutation-resolvers.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const s3 = require('../utils/config');
+const { promisify } = require('util');
+const { extname } = require('path');
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+
+class MutationResolver {
+	constructor() {
+		this.s3 = s3;
+	}
+
+	//create a bucket.
+	async createBucket(bucketName) {}
+
+	//upload object.
+	async uploadObject(file, bucketName) {}
+
+	//upload objects.
+	async uploadObjects(files, bucketName) {}
+
+	//delete object.
+	async deleteObject(bucketName, key) {}
+
+	//delete objects.
+	async deleteObjects(bucketName, objectKeys) {}
+
+	//delete bucket.
+	async deleteBucket(bucketName) {}
+}
+
+module.exports = MutationResolver;
--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,28 @@
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
-module.exports = { typeDefs, resolvers };
+const { gql } = require('apollo-server');
+
+const Object = require('./object');
+const Response = require('./response');
+
+const Query = gql`
+	type Query {
+		fetchBuckets: [String!]!
+		fetchObjects(bucketName: String): [Object!]!
+	}
+`;
+
+const Mutation = gql`
+	type Mutation {
+		createBucket(bucketName: String!): Response
+		uploadObject(file: Upload!, bucketName: String!): Object
+		uploadObjects(files: [Upload!]!, bucketName: String!): [Object!]!
+		deleteObject(bucketName: String!, key: String!): Response
+		deleteObjects(bucketName: String!, objectKeys: [String!]!): Response
+
+		deleteBucket(bucketName: String!): Response
+	}
+`;
+
+module.exports = { typeDefs, resolvers, Object, Response, Query, Mutation };
