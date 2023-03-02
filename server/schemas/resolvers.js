2c58cf5eae0a4ff8ebc189760cdf1ae9199fabfa
Ralakash <shawnkaralash@gmail.com>
2023-03-02 18:30:09 -0500

    gets rid of unnecessary s3 stuff

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,60 +1,59 @@
-const { AuthenticationError } = require("apollo-server-express");
-const  User  = require("../models/User");
-const { signToken } = require("../utils/auth");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
-
+const { AuthenticationError } = require('apollo-server-express');
+const User = require('../models/User');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const resolvers = {
-  Query: {
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: "orders.products",
-          populate: "category",
-        });
+	Query: {
+		user: async (parent, args, context) => {
+			if (context.user) {
+				const user = await User.findById(context.user._id).populate({
+					path: 'orders.products',
+					populate: 'category',
+				});
 
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+				user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
-        return user;
-      }
+				return user;
+			}
 
-      throw new AuthenticationError("Not logged in");
-    },
+			throw new AuthenticationError('Not logged in');
+		},
 
-    // Mutation: {
-    //   addUser: async (parent, args) => {
-    //     const user = await User.create(args);
-    //     const token = signToken(user);
+		// Mutation: {
+		//   addUser: async (parent, args) => {
+		//     const user = await User.create(args);
+		//     const token = signToken(user);
 
-    //     return { token, user };
-    //   },
-    //   updateUser: async (parent, args, context) => {
-    //     if (context.user) {
-    //       return await User.findByIdAndUpdate(context.user._id, args, {
-    //         new: true,
-    //       });
-    //     }
+		//     return { token, user };
+		//   },
+		//   updateUser: async (parent, args, context) => {
+		//     if (context.user) {
+		//       return await User.findByIdAndUpdate(context.user._id, args, {
+		//         new: true,
+		//       });
+		//     }
 
-    //     throw new AuthenticationError("Not logged in");
-    //   },
-    //   login: async (parent, { email, password }) => {
-    //     const user = await User.findOne({ email });
+		//     throw new AuthenticationError("Not logged in");
+		//   },
+		//   login: async (parent, { email, password }) => {
+		//     const user = await User.findOne({ email });
 
-    //     if (!user) {
-    //       throw new AuthenticationError("Incorrect credentials");
-    //     }
+		//     if (!user) {
+		//       throw new AuthenticationError("Incorrect credentials");
+		//     }
 
-    //     const correctPw = await user.isCorrectPassword(password);
+		//     const correctPw = await user.isCorrectPassword(password);
 
-    //     if (!correctPw) {
-    //       throw new AuthenticationError("Incorrect credentials");
-    //     }
+		//     if (!correctPw) {
+		//       throw new AuthenticationError("Incorrect credentials");
+		//     }
 
-    //     const token = signToken(user);
+		//     const token = signToken(user);
 
-    //     return { token, user };
-    //   },
-    // },
-  },
+		//     return { token, user };
+		//   },
+		// },
+	},
 };
 
 module.exports = resolvers;
