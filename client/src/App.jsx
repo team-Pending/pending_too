5743054b5b4b9a1550e271247ab0fd1f807a7ef5
Ralakash <shawnkaralash@gmail.com>
2023-03-06 10:44:35 -0500

    merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 // Laura adding to complete route for auth
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { createHttpLink } from "@apollo/client/link/http";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { AuthProvider } from './utils/auth';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 import Navbar from "./components/navbar/Nav";
@@ -14,63 +18,53 @@ import "./app.css";
 import Admin from "./components/admin/Admin.jsx";
 import LoginForm from "./components/account/LoginForm";
 import CategoryMenu from "./components/CategoryMenu";
-import Navbar from './components/navbar/Nav';
-import ShopCard from './components/ShopCard/ShopCard';
-import About from './components/About';
-import Account from './components/account/Account';
-import './app.css';
-import Admin from './components/admin/Admin.jsx';
-import Edit from './components/account/Edit';
-import LoginForm from './components/account/LoginForm';
-import NotFound from './components/NotFound.jsx';
 
 // Laura adding regarding pulling auth path
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-	uri: '/graphql',
-	//credentials: 'include'
+  uri: '/graphql',
+  //credentials: 'include'
 });
 
 // Construct link that will attach the JWT token to every request.
 const authLink = setContext((_, { headers }) => {
-	// get the authentication token from local storage if it exists
-	const token = localStorage.getItem('id_token');
-	// return the headers to the context so httpLink can read them
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : '',
-		},
-	};
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 const client = new ApolloClient({
-	// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-	// link: authLink.concat(httpLink),
-	cache: new InMemoryCache(),
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  // link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
-	return (
-		<AuthProvider>
-			<ApolloProvider client={client}>
-				<>
-					<div>
-						<Navbar />
+  return (
+    <ApolloProvider client={client}>
+      <>
+        <div>
+          <Navbar />
 
-						<div className="container">
-							<Routes>
-								<Route path="/" element={<ShopCard />} />
-								<Route path="/about" element={<LoginForm />} />
-								<Route path="/account" element={<Account />} />
-								<Route path="/admin" element={<Admin />} />
-								<Route path="/*" element={<NotFound />} />
-							</Routes>
-						</div>
-					</div>
-				</>
-			</ApolloProvider>
-		</AuthProvider>
-	);
+
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<ShopCard />} />
+        <Route path="/about" element={<LoginForm />} />
+        <Route path='/account' element={<Account />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+      </div>
+
+        </div>
+      </>
+    </ApolloProvider>
+  );
 }
 
 export default App;
