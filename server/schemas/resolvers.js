b0773091fb2f1f4a866afb6143bf90019a35f03c
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2023-03-07 15:30:35 -0500

    added/commented out the order mutation in resolvers, finished model export for Order

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,161 +1,183 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Category, Order, Product, User} = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { Category, Order, Product, User } = require("../models");
+const { signToken } = require("../utils/auth");
 // const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-const { GraphQLError } = require('graphql');
+const { GraphQLError } = require("graphql");
 
-const AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR';
+const AUTHENTICATION_ERROR = "AUTHENTICATION_ERROR";
 
 const resolvers = {
-	Query: {
-		user: async (parent, args, context) => {
-			//added for sake of testing without login
-			context.user = true;
-			if (context.user) {
-				const user = await User.find();
+  Query: {
+    user: async (parent, args, context) => {
+      //added for sake of testing without login
+      context.user = true;
+      if (context.user) {
+        const user = await User.find();
 
-				return user;
-			}
+        return user;
+      }
 
-			throw new AuthenticationError('Not logged in');
-		},
-		// Laura added to pull adminUser info
-		adminUser: async (_, __, context) => {
-			//added for sake of testing without login
-			context.adminUser = true;
-			const user = await User.find();
-			return user;
-			// throw new AuthenticationError('Not logged in');
-		},
+      throw new AuthenticationError("Not logged in");
+    },
+    // Laura added to pull adminUser info
+    adminUser: async (_, __, context) => {
+      //added for sake of testing without login
+      context.adminUser = true;
+      const user = await User.find();
+      return user;
+      // throw new AuthenticationError('Not logged in');
+    },
 
-		adminProduct: async (_, __, context) => {
-			//added for sake of testing without login
-			context.adminProduct = true;
-			const product = await Product.find();
-			return product;
-			// throw new AuthenticationError('Not logged in');
-		},
+    adminProduct: async (_, __, context) => {
+      //added for sake of testing without login
+      context.adminProduct = true;
+      const product = await Product.find();
+      return product;
+      // throw new AuthenticationError('Not logged in');
+    },
 
-		product: async (parent, args, context) => {
-			//added for sake of testing without login
-			context.product = true;
-			if (context.product) {
-				const product = await Product.find();
+    product: async (parent, args, context) => {
+      //added for sake of testing without login
+      context.product = true;
+      if (context.product) {
+        const product = await Product.find();
 
-				return product;
-			}
+        return product;
+      }
 
-			throw new AuthenticationError('Not logged in');
-		},
-		products: async (parent, args, context) => {
-			return Product.find({ productName: { $regex: args.name, $options: 'i' } });
-		},
-		// order: async (parent, { _id }, context) => {
-		//   if (context.user) {
-		// 	const user = await User.findById(context.user._id).populate({
-		// 	  path: 'orders.products',
-		// 	  populate: 'category'
-		// 	});
-	
-		// 	return user.orders.id(_id);
-		//   }
-	
-		//   throw new AuthenticationError('Not logged in');
-		// },
-		// checkout: async (parent, args, context) => {
-		//   const url = new URL(context.headers.referer).origin;
-		//   const order = new Order({ products: args.products });
-		//   const line_items = [];
-	
-		//   const { products } = await order.populate('products');
-	
-		//   for (let i = 0; i < products.length; i++) {
-		// 	const product = await stripe.products.create({
-		// 	  name: products[i].name,
-		// 	  description: products[i].description,
-		// 	  images: [`${url}/images/${products[i].image}`]
-		// 	});
-	
-		// 	const price = await stripe.prices.create({
-		// 	  product: product.id,
-		// 	  unit_amount: products[i].price * 100,
-		// 	  currency: 'usd',
-		// 	});
-	
-		// 	line_items.push({
-		// 	  price: price.id,
-		// 	  quantity: 1
-		// 	});
-		//   }
-	
-		//   const session = await stripe.checkout.sessions.create({
-		// 	payment_method_types: ['card'],
-		// 	line_items,
-		// 	mode: 'payment',
-		// 	success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-		// 	cancel_url: `${url}/`
-		//   });
-	
-		//   return { session: session.id };
-		// }
-	},
-	Mutation: {
-		login: async (parent, { email, password }) => {
-			const user = await User.findOne({ email });
-			if (!user) {
-				throw new GraphQLError('User does not exist!', {
-					extensions: {
-						code: AUTHENTICATION_ERROR,
-					},
-				});
-			}
-			const isPasswordCorrect = await user.isCorrectPassword(password);
-			if (!isPasswordCorrect) {
-				throw new GraphQLError('Incorrect Password!', {
-					extensions: {
-						code: AUTHENTICATION_ERROR,
-					},
-				});
-			}
-			const token = signToken({
-				email: email,
-			});
-			return { token };
-		},
+      throw new AuthenticationError("Not logged in");
+    },
+    products: async (parent, args, context) => {
+      return Product.find({
+        productName: { $regex: args.name, $options: "i" },
+      });
+    },
+    // order: async (parent, { _id }, context) => {
+    //   if (context.user) {
+    // 	const user = await User.findById(context.user._id).populate({
+    // 	  path: 'orders.products',
+    // 	  populate: 'category'
+    // 	});
 
-		addUser: async (parent, { username, firstName, lastName, email, password }) => {
-			const checkUser = await User.findOne({ email });
-			if (checkUser) {
-				console.log('email already exists');
-				throw new GraphQLError('A user with this email already exists!', {
-					extensions: {
-						code: AUTHENTICATION_ERROR,
-					},
-				});
-			} else {
-				const newUser = await new User({
-					email: email,
-					username: username,
-					firstName: firstName,
-					lastName: lastName,
-					password: password,
-				});
-				await newUser.save();
-			}
-			const token = signToken({
-				email: email,
-			});
-			return { token };
-		},
+    // 	return user.orders.id(_id);
+    //   }
 
-		updateUser: async (parent, args, context) => {
-			if (context.user) {
-			  return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-			}
-	  
-			throw new AuthenticationError('Must be logged in');
-		  },
-	},
+    //   throw new AuthenticationError('Not logged in');
+    // },
+    // checkout: async (parent, args, context) => {
+    //   const url = new URL(context.headers.referer).origin;
+    //   const order = new Order({ products: args.products });
+    //   const line_items = [];
+
+    //   const { products } = await order.populate('products');
+
+    //   for (let i = 0; i < products.length; i++) {
+    // 	const product = await stripe.products.create({
+    // 	  name: products[i].name,
+    // 	  description: products[i].description,
+    // 	  images: [`${url}/images/${products[i].image}`]
+    // 	});
+
+    // 	const price = await stripe.prices.create({
+    // 	  product: product.id,
+    // 	  unit_amount: products[i].price * 100,
+    // 	  currency: 'usd',
+    // 	});
+
+    // 	line_items.push({
+    // 	  price: price.id,
+    // 	  quantity: 1
+    // 	});
+    //   }
+
+    //   const session = await stripe.checkout.sessions.create({
+    // 	payment_method_types: ['card'],
+    // 	line_items,
+    // 	mode: 'payment',
+    // 	success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+    // 	cancel_url: `${url}/`
+    //   });
+
+    //   return { session: session.id };
+    // }
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new GraphQLError("User does not exist!", {
+          extensions: {
+            code: AUTHENTICATION_ERROR,
+          },
+        });
+      }
+      const isPasswordCorrect = await user.isCorrectPassword(password);
+      if (!isPasswordCorrect) {
+        throw new GraphQLError("Incorrect Password!", {
+          extensions: {
+            code: AUTHENTICATION_ERROR,
+          },
+        });
+      }
+      const token = signToken({
+        email: email,
+      });
+      return { token };
+    },
+
+    addUser: async (
+      parent,
+      { username, firstName, lastName, email, password }
+    ) => {
+      const checkUser = await User.findOne({ email });
+      if (checkUser) {
+        console.log("email already exists");
+        throw new GraphQLError("A user with this email already exists!", {
+          extensions: {
+            code: AUTHENTICATION_ERROR,
+          },
+        });
+      } else {
+        const newUser = await new User({
+          email: email,
+          username: username,
+          firstName: firstName,
+          lastName: lastName,
+          password: password,
+        });
+        await newUser.save();
+      }
+      const token = signToken({
+        email: email,
+      });
+      return { token };
+    },
+
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, {
+          new: true,
+        });
+      }
+
+      throw new AuthenticationError("Must be logged in");
+    },
+
+    // addOrder: async (parent, { products }, context) => {
+    //   console.log(context);
+    //   if (context.user) {
+    //     const order = new Order({ products });
+
+    //     await User.findByIdAndUpdate(context.user._id, {
+    //       $push: { orders: order },
+    //     });
+
+    //     return order;
+    //   }
+
+    //   throw new AuthenticationError("Must be logged in");
+    // },
+  },
 };
 
 module.exports = resolvers;
