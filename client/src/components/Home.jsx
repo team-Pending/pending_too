e8018dcbb401e25422f344b1271a1a7625834571
JCool221 <109809337+JCool221@users.noreply.github.com>
2023-03-07 19:43:38 -0500

    fixed the render errors cards now render but without uncalled content

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,25 +11,17 @@ function Home() {
   const { data, loading } = useQuery(QUERY_SEARCH_PRODUCT, {
     variables: { name: search },
   });
-  console.log(data);
+  if (loading) {
+    return <p>Loading...</p>
+  }
   const content = data?.products;
   console.log(content)
-  return content.map((content) => {
+  return content.map((item) => {
     return (
-      <>
-
-        <ShopCard key={content.id} content={content} />
-      </>
+        <ShopCard key={item.id} content={item} />
     );
   });
 
-  // return (
-  //   <div>
-  //     <Search search={search} handleSearch={setSearch}/>
-  //     <h1>{search}</h1>
-  //     <pre>{JSON.stringify(content, null, 2 )}</pre>
-  //     </div>
-  //     );
 }
 
 export default Home;
--- client/src/components/ShopCard/ShopCard.jsx
+++ client/src/components/ShopCard/ShopCard.jsx
@@ -4,16 +4,16 @@ import LikeDislike from "../LikeDislike/LikeDislike";
 function ShopCard({content}) {
     return (
       <section
-        key={content.index}
+      key={content.id}
         className="card"
         style={{ backgroundImage: `url(${content.image})` }}
       >
         <h1>{content.username}</h1>
-        <h2>{content.title}</h2>
+        <h2>{content.productName}</h2>
         <ul className="info">
           <li>{content.price}</li>
           <li>{content.username}</li>
-          <li>{content.information}</li>
+          <li>{content.productDescription}</li>
           <li>
             <button className="add-to-cart">Add to cart</button>
             {/* <button className="remove-from-cart">Remove from cart</button> */}
@@ -22,7 +22,7 @@ function ShopCard({content}) {
         <ul className="like">
           <LikeDislike />
         <div className="like-count">
-          <p>{content.likes}</p>
+          <p>{content.rating}</p>
         </div>
         </ul>
       </section>
@@ -30,3 +30,10 @@ function ShopCard({content}) {
   }
 
 export default ShopCard;
+  // return (
+  //   <div>
+  //     <Search search={search} handleSearch={setSearch}/>
+  //     <h1>{search}</h1>
+  //     <pre>{JSON.stringify(content, null, 2 )}</pre>
+  //     </div>
+  //     );
