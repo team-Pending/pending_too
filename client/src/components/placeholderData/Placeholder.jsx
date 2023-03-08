3a38786bb6ff95a95b7993821ea00cd3b1a5a2e2
Jcool221 <jcool2939@gmail.com>
2023-03-08 13:01:01 -0500

    working on stuff

---
+++ client/src/components/placeholderData/Placeholder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Placeholder() {
+  return (
+    <div>
+        <h1>here's the placeholder</h1>
+      <h1>the target is {params.target}</h1>
+    </div>
+  );
+}
+
+export default Placeholder;
+
