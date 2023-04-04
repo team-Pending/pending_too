001a05fa4055d0457344eb37239d0137a45068d9
shawn karalash <shawnkaralash@gmail.com>
2023-04-04 19:08:30 -0400

    fixes some issues with uploading new products but is not recognizing itemPrice as being sent or as a float

--- client/src/utils/mutations.jsx
+++ client/src/utils/mutations.jsx
@@ -1,93 +1,91 @@
 import { gql } from '@apollo/client';
 
 export const ADD_ORDER = gql`
-	mutation addOrder($products: [ID]!) {
-		addOrder(products: $products) {
-			purchaseDate
-			products {
-				_id
-				name
-				description
-				price
-				quantity
-				category {
-					name
-				}
-			}
-		}
-	}
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      purchaseDate
+      products {
+        _id
+        name
+        description
+        price
+        quantity
+        category {
+          name
+        }
+      }
+    }
+  }
 `;
 
 export const ADD_PRODUCT = gql`
-	mutation addProduct(
-		$email: String!
-		$productName: String!
-		$productDescription: String!
-		$price: Float!
-		$s3Key: String!
-	) {
-		addProduct(
-			email: $email
-			productName: $productName
-			productDescription: $productDescription
-			price: $price
-			s3key: $s3key
-		) {
-			success
-			message
-		}
-	}
+  mutation addProduct(
+    $email: String!
+    $productName: String!
+    $productDescription: String!
+    $price: Float!
+    $s3Key: String!
+  ) {
+    addProduct(
+      email: $email
+      productName: $productName
+      productDescription: $productDescription
+      price: $price
+      s3key: $s3Key
+    ) {
+      firstName
+    }
+  }
 `;
 
 export const DELETE_PRODUCT = gql`
-	mutation deleteProduct($productId: ID!) {
-		deleteProduct(Id: $productId) {
-			id
-			productName
-		}
-	}
+  mutation deleteProduct($productId: ID!) {
+    deleteProduct(Id: $productId) {
+      id
+      productName
+    }
+  }
 `;
 
 export const ADD_USER = gql`
-	mutation addUser(
-		$username: String!
-		$firstName: String!
-		$lastName: String!
-		$email: String!
-		$password: String!
-	) {
-		addUser(
-			username: $username
-			firstName: $firstName
-			lastName: $lastName
-			email: $email
-			password: $password
-		) {
-			token
-			user {
-				_id
-			}
-		}
-	}
+  mutation addUser(
+    $username: String!
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
 `;
 
 export const DELETE_USER = gql`
-  mutation deleteUser(
-    $_id: ID!) {
-      deleteUser(_id: $_id){
-        success
-        message
-      }
+  mutation deleteUser($_id: ID!) {
+    deleteUser(_id: $_id) {
+      success
+      message
     }
-  `;
+  }
+`;
 
 export const LOGIN = gql`
-	mutation login($email: String!, $password: String!) {
-		login(email: $email, password: $password) {
-			token
-			user {
-				_id
-			}
-		}
-	}
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
 `;
